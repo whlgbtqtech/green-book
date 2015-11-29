3ed1a667a41b92a281598f662638d52c5ab1816a
Kate Jefferson <barbedbella@gmail.com>
2015-11-29 14:57:29 -0800

    mocked up home page

---
+++ Public/controllers/main-controller.js
@@ -0,0 +1,16 @@
+(function() {
+
+    angular.module('gb')
+        .controller('Main.Controller', MainController);
+
+    MainController.$inject = [];
+
+    function MainController() {
+
+        var vm = this;
+
+
+
+    }
+
+})();
