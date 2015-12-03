6e9f14c7c4d0ebb182d99b674d98668f84a84d60
Courtney Wilburn <courtney@cjwilburn.com>
2015-12-02 22:45:03 -0500

    citizen complaints

--- app/models/citizen-complaint.js
+++ app/models/citizen-complaint.js
@@ -7,10 +7,51 @@ var mongoosePaginate = require('mongoose-paginate');
 exports = module.exports = function(mongoose, iglooMongoosePlugin) {
 
   var CitizenComplaint = new mongoose.Schema({
-    name: {
-      type: String,
-      required: true
-    }
+      ComplaintType:  {
+          type: String
+      },
+      CCACaseNumber: {
+          type: String
+      },
+      DateReceived: {
+          type: Date
+      },
+      HowReceived: {
+          type: String
+      },
+      District: {
+          type: String
+      },
+      Neighborhood: {
+          type: String
+      },
+      allegation-code: {
+          type: String
+      },
+      description: {
+          type: String
+      },
+      disposition-code: {
+          type: String
+      },
+      respondent-id: {
+          type: String
+      },
+      OfficerSex: {
+          type: String
+      },
+      OfficerRace: {
+          type: String
+      },
+      ComplainantSex: {
+          type: String
+      },
+      ComplainantRace: {
+          type: String
+      },
+      HowClosed:{
+          type: String
+      }
   });
 
   // virtuals
